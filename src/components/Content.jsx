d2cbbc4ce11071a6701cc1dadfba5900a7ac8e23
allysonweb2020 <santanaallyson63@gmail.com>
2025-03-26 10:48:31 -0300

    Add files via upload

---
+++ src/components/Content.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Content.css';
+
+function Content()
+    {
+        return(
+            <>
+            <section>
+            <h2>Diagrama Entidade-Relacionamento (DER)</h2>
+            <p>
+O Diagrama Entidade-Relacionamento (DER) é uma representação gráfica que descreve a estrutura de um banco de dados, destacando as entidades, seus atributos e os relacionamentos entre elas. Ele é uma ferramenta usada na modelagem de dados.
+Para que serve?
+Planejamento de Banco de Dados: Auxilia no design de sistemas de banco de dados, ajudando a organizar as informações de forma lógica e eficiente.
+Visualização de Dados: Facilita a visualização das entidades, atributos e relacionamentos, tornando o entendimento do modelo de dados mais claro.
+Evitar Redundâncias: Ajuda a estruturar dados de maneira que se evite a duplicação e garanta que o banco de dados seja eficiente.
+Quem usa?
+Analistas de Sistemas: Para criar o modelo de dados de um sistema.
+Desenvolvedores de Banco de Dados: Para implementar e otimizar a estrutura do banco de dados.
+Engenheiros de Dados: Para criar e organizar os dados de forma estruturada.
+Arquitetos de Software: Para planejar como os dados serão armazenados e acessados no sistema.
+            </p>
+            <iframe src="https://www.youtube.com/embed/z7AMc4a9xFE?si=V8lh9lESffIW4Ln-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+            </section>
+            </>
+        );
+    };
+    export default Content;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+function Header(){
+    return(
+        <>
+        <header> 
+
+        <h1>Diagrama (DER)</h1>
+
+        </header>
+        </>
+
+    );
+
+};
+export default Header;
